0ed610ae34ab0efdac86343ef6953c470fdfeff6
mlorenzoo <malool@fp.insjoaquimmir.cat>
2023-10-06 16:25:09 +0200

    Documentació actualitzada

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, g
+
+DATABASE = 'database.db'
+
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+	return db
+
 
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
 	return render_template('app.html')
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def student():
+   return render_template('student.html')
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+
+if __name__ == 'main':
+   app.run(debug = True)
