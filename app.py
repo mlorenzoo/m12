f7ca4f88c6b84acf6bea566cf13af60245ce78e6
mlorenzoo <malool@fp.insjoaquimmir.cat>
2023-10-06 19:29:05 +0200

    Documentació actualitzada

--- app.py
+++ app.py
@@ -7,34 +7,25 @@ def get_db():
 	db = getattr(g, '_database', None)
 	if db is None:
 		db = g._database = sqlite3.connect(DATABASE)
+		db.row_factory = sqlite3.Row
 	return db
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def hello():
 	return render_template('app.html')
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-@app.route('/')
-def student():
-   return render_template('student.html')
-
-
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
-
+@app.route("/products/list")
+def nose():
+	products = query_db('select * from products')
+	return render_template("list.html", products = products)
 
 if __name__ == 'main':
    app.run(debug = True)
